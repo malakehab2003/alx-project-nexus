e00f27101b0d90ad8a16cc7a331adec1954ef4f9
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-03-06 19:17:00 +0200

    add brand apis

--- Clothes_app/Product/api/brand.py
+++ Clothes_app/Product/api/brand.py
@@ -2,24 +2,24 @@ from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from ..models import Category, Product
-from ..serialzers import CategorySerializer, ProductSerializer
+from ..models import Brand, Product
+from ..serialzers import BrandSerializer, ProductSerializer
 
 
-class CategoryViewSet(mixins.RetrieveModelMixin,
+class BrandViewSet(mixins.RetrieveModelMixin,
                        viewsets.GenericViewSet):
-    """ViewSet for the Category model."""
-    queryset = Category.objects.all()
+    """ViewSet for the Brand model."""
+    queryset = Brand.objects.all()
     lookup_field = "pk"
-    serializer_class = CategorySerializer
+    serializer_class = BrandSerializer
     permission_classes = [IsAuthenticated]
     
     def retrieve(self, request, *args, **kwargs):
-        category_id = kwargs.get("pk")
+        brand_id = kwargs.get("pk")
 
-        if not category_id:
+        if not brand_id:
             return Response({"error": "required id"}, status=status.HTTP_400_BAD_REQUEST)
         
-        products = Product.objects.filter(category=category_id)
+        products = Product.objects.filter(brand=brand_id)
         serializer = ProductSerializer(products, many=True)
         return Response({"products": serializer.data}, status=status.HTTP_200_OK)
--- Clothes_app/Product/urls.py
+++ Clothes_app/Product/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api.product import ProductViewSet
 from .api.category import CategoryViewSet
+from .api.brand import BrandViewSet
 
 router = DefaultRouter()
 router.register("product", ProductViewSet)
 router.register("category", CategoryViewSet)
+router.register("brand", BrandViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
