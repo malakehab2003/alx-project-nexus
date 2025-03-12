1cfb1cb856f56b5d4ece954c2101640c972f8af8
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-03-12 21:03:18 +0200

    try deploy

--- Clothes_app/Clothes_app/settings.py
+++ Clothes_app/Clothes_app/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-h4u2^n@)5=yved+kpb63hzd=^b%8vp54n@gcrvyj6fc#b)4f#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['MalakEhab.pythonanywhere.com', 'localhost', 'alx-project-nexus-production.up.railway.app']
+ALLOWED_HOSTS = [
+    'MalakEhab.pythonanywhere.com',
+    'localhost',
+    'alx-project-nexus-production.up.railway.app'
+]
 
 
 # Application definition
