3ba58024159d7a2fbee5bafe016e74b2a857d22d
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-03-13 03:55:21 +0200

    try deploy

--- Clothes_app/Clothes_app/settings.py
+++ Clothes_app/Clothes_app/settings.py
@@ -124,6 +124,7 @@ CACHES = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -208,7 +209,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = "/static/"
+
+# Set STATIC_ROOT for collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Storage backend for serving static files
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
